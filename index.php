8c2b9ea2eab5a0a4b8b7d4219b69dee7ab665bd8
ShrykeAbysmal <keith.aich@organizdkaoz.com>
2021-05-16 13:56:39 -0700

    More stuff to index

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <? /* Main php file */ ?>
+<html>
+<head>
+<title>Hearthstone Card Retrieval</title>
+</head>
+<body bgcolor="FFFFFF">
+<h1>Welcome to Hearthstone Card Retrieval</h1>
+</body>
+</html>
